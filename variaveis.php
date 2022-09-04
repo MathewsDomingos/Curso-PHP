f12f7b503d2ef947beb865466d3cacc4b7e5e345
Mateus Domingos Felipe <mateusdomingos.etec@gmail.com>
2022-09-04 15:14:16 -0300

    Variaveis em PHP

---
+++ variaveis.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aula 04</title>
+  <link rel="stylesheet" href="Aulas/php-aula04/aula04/_css/estilo.css">
+</head>
+<body>
+
+  <div>
+    <div>
+        <?php 
+        $n = 4;
+        $no = "Mateus Domingos";
+        $idade = 22;
+        echo "Meu nome é: " .$no . ", e tenho " . $idade . " anos.";
+        
+        ?>
+      </div>
+  </div>
+  
+</body>
+</html>
